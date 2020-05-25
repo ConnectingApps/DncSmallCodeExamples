83c12454120e62587b9d8c2ad049d47a582c4852
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-25 17:24:28 +0200

    HttpClient demo added

---
+++ ConnectingApps.DncSmallCodeExamples/ConnectingApps.HttpClientDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConnectingApps.HttpClientDemo
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using (var httpClient = new HttpClient(new SafeHandler()))
+            {
+                httpClient.BaseAddress = new Uri("https://www.google.com/");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "DummyToken");
+                var response = await httpClient.GetAsync("");
+                Console.WriteLine($"Returned status code: {response.StatusCode}");
+                Console.WriteLine("END");
+            }
+        }
+    }
+}
---
+++ ConnectingApps.DncSmallCodeExamples/ConnectingApps.HttpClientDemo/SafeHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConnectingApps.HttpClientDemo
+{
+    public class SafeHandler : DelegatingHandler
+    {
+        public SafeHandler()
+        {
+            InnerHandler = new HttpClientHandler();
+        }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var result = base.SendAsync(request, cancellationToken);
+
+            if (request.Headers.Authorization != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue(request.Headers.Authorization.Scheme, "***");
+            }
+
+            var toLog = JsonConvert.SerializeObject(request);
+            Console.WriteLine(toLog);
+            return result;
+        }
+    }
+}
